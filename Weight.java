ec908ad37210abf038a7201b68e693a18efe7081
לברן <yishai.levran@gmail.com>
2025-05-23 10:58:12 +0300

    make a Weight class

---
+++ Weight.java
@@ -0,0 +1,18 @@
+public class Weight {
+    private int kg;
+    private int grams;
+
+    public Weight(int kg, int grams) {
+        this.kg = kg;
+        this.grams = grams;
+
+        if (grams > 999) {
+            this.kg++;
+            this.grams = this.grams - 1000;
+        }
+    }
+    public String toString(){
+        return this.kg+" kg and "+this.grams+" gr.";
+    }
+
+}
